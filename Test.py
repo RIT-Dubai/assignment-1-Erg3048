11005f9bee6ce0f612f306bcf9e4e6426866b306
Erg3048 <90134618+Erg3048@users.noreply.github.com>
2021-10-06 21:15:54 +0400

    Developed test for draw shape rectangle

--- Test.py
+++ Test.py
@@ -7,3 +7,14 @@ def test_rectangle_will_fit():
     result = main.rectangle_will_fit(100, 100, 70, 50)
     assert result is not False, "Rectangle does not fit"
 
+
+def test_draw_shape_rectangle():
+    x = 0
+    y = 0
+    perimeter = main.draw_shape('rectangle', 'red', 100, 100, 70, 50)
+    assert t.fillcolor() == 'red', "color is incorrect"
+    assert t.xcor() == x, "x coordinate is incorrect"
+    assert t.ycor() == y, "y coordinate is incorrect"
+    assert t.heading() == 0, "turtle not facing East"
+    p = 2*(70+50)
+    assert perimeter == p, "Perimeter is incorrect"
